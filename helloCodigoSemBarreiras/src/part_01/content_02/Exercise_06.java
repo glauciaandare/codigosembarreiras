6c2d826da35004e7b9dec5cbed449795eb46685d
Gláucia Andare <glaucia.andare@gmail.com>
2021-09-01 15:42:07 -0300

    Resolução do Exercise_06

--- helloCodigoSemBarreiras/src/part_01/content_02/Exercise_06.java
+++ helloCodigoSemBarreiras/src/part_01/content_02/Exercise_06.java
@@ -7,5 +7,26 @@
 
 package part_01.content_02;
 
+import java.util.Scanner;
+
 public class Exercise_06 {
+
+    public static void main(String[] args) {
+        Scanner enter = new Scanner(System.in);
+
+        System.out.println(" Jogador 1 - Digite 0 para PEDRA, 1 para PAPEL ou 2 para TESOURA: ");
+        int jogador1 = enter.nextInt();
+
+        System.out.println("Jogador 2 - Digite 0 para PEDRA, 1 para PAPEL ou 2 para TESOURA: ");
+        int jogador2 = enter.nextInt();
+
+        if (jogador1 == jogador2)
+            System.out.println("Empate");
+        else if (jogador1 - jogador2 == -2 || jogador1 - jogador2 == 1)
+            System.out.println("Jogador 1 venceu!");
+        else
+            System.out.println("Jogador 2 venceu!");
+
+        enter.close();
+    }
 }
