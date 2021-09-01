798db590d864dac10f254e8e742a25d676d6877c
Gláucia Andare <glaucia.andare@gmail.com>
2021-09-01 16:16:43 -0300

    Resolução dos exercicios da parte 3

--- helloCodigoSemBarreiras/src/part_01/content_03/Exercise_01.java
+++ helloCodigoSemBarreiras/src/part_01/content_03/Exercise_01.java
@@ -9,6 +9,40 @@
 
 package part_01.content_03;
 
+import java.util.Scanner;
+
 public class Exercise_01 {
+    public static void main(String[] args) {
+        int maiorNumero = Integer.MIN_VALUE;
+        int menorNumero = Integer.MAX_VALUE;
+
+        double somaDosNumeros = 0;
+        int numeroCorrente = 0;
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        int quantiadeDeNumeros = 3;
+
+        for (int contador = 1; contador <=quantiadeDeNumeros; contador++) {
+            System.out.printf("Digite o %dº número", contador);
+
+            numeroCorrente = scanner.nextInt();
+
+            if (numeroCorrente > maiorNumero) {
+                maiorNumero = numeroCorrente;
+            }
+
+            if (numeroCorrente < menorNumero) {
+                menorNumero = numeroCorrente;
+            }
+
+            somaDosNumeros = somaDosNumeros + numeroCorrente + 0.0;
+        }
+
+        System.out.println("Maior número " + maiorNumero);
+        System.out.println("Menor número " + menorNumero);
+        System.out.println("Média " + somaDosNumeros / quantiadeDeNumeros);
+    }
 
 }
--- helloCodigoSemBarreiras/src/part_01/content_03/Exercise_02.java
+++ helloCodigoSemBarreiras/src/part_01/content_03/Exercise_02.java
@@ -4,5 +4,27 @@
 
 package part_01.content_03;
 
+import java.util.Scanner;
+
 public class Exercise_02 {
+
+    public static void main(String[] args) {
+        Scanner enter = new Scanner(System.in);
+
+        System.out.println("Qual o número que deseja saber o fatorial?");
+        double numero = enter.nextInt();
+
+        long fatorial = 1;
+
+        while (numero > 1) {
+             fatorial = (long) (fatorial * numero);
+            numero = numero - 1;
+        }
+
+        System.out.printf("O fatorial é %d", fatorial);
+
+        enter.close();
+    }
+
 }
+
